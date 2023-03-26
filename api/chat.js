2cea82dce02bbbd4b75566500b6f0483b082ccd1
lunoshist <lucas.noirie@epitech.digital>
2023-03-26 12:37:22 +0200

    fourth version

--- api/chat.js
+++ api/chat.js
@@ -1,19 +1,14 @@
 const apiKey = process.env.API_KEY;
 
 export default (req, res) => {
-    try {
-        const body = req.body;
-        const response = await fetch("https://api.openai.com/v1/chat/completions", {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${apiKey}`,
-            },
-            body: body
-        });
-        res.json(response);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-      }
+    const body = req.body;
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${apiKey}`,
+        },
+        body: body
+    });
+    res.json(response);
 }
