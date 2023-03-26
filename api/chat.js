0136de8ea9e1cf3a8200e9c2687f53ed8c0df11a
lunoshist <lucas.noirie@epitech.digital>
2023-03-26 13:00:16 +0200

    fifth version

--- api/chat.js
+++ api/chat.js
@@ -1,14 +1,25 @@
 const apiKey = process.env.API_KEY;
+const { Configuration, OpenAIApi } = require('openai');
 
-export default (req, res) => {
-    const body = req.body;
-    const response = await fetch("https://api.openai.com/v1/chat/completions", {
-        method: 'POST',
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${apiKey}`,
-        },
-        body: body
-    });
-    res.json(response);
+const configuration = new Configuration({
+    apiKey: process.env.API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+module.exports = async (req, res) => {
+    try {
+        const body = req.body;
+        const response = await openai
+            .createChatCompletion({
+                model: 'gpt-3.5-turbo',
+                messages: body,
+            })
+            .catch((error) => {
+                console.log(`OPENAI ERR: ${error}`);
+            });
+        res.json(response);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+      }
 }
