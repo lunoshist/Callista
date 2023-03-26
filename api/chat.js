9a78c8ab726fe0a466a90add2ac5725693e8807e
lunoshist <lucas.noirie@epitech.digital>
2023-03-26 14:25:57 +0200

    sixth version

--- api/chat.js
+++ api/chat.js
@@ -8,7 +8,14 @@ const openai = new OpenAIApi(configuration);
 module.exports = async (req, res) => {
     try {
         const body = req.body;
-        
+        const response = await fetch("https://api.openai.com/v1/chat/completions", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${apiKey}`,
+            },
+            body: body
+        });
         res.json(body);
       } catch (error) {
         console.error(error);
