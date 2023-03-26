29ad68d06034740bc0398b4f12d8164d44ca2841
lunoshist <lucas.noirie@epitech.digital>
2023-03-26 14:20:59 +0200

    sixth version

--- api/chat.js
+++ api/chat.js
@@ -8,15 +8,8 @@ const openai = new OpenAIApi(configuration);
 module.exports = async (req, res) => {
     try {
         const body = req.body;
-        const response = await openai
-            .createChatCompletion({
-                model: 'gpt-3.5-turbo',
-                messages: body,
-            })
-            .catch((error) => {
-                console.log(`OPENAI ERR: ${error}`);
-            });
-        res.json(response);
+        
+        res.json(body);
       } catch (error) {
         console.error(error);
         res.status(500).json({ message: 'Internal server error' });
