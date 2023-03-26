ef9945ba8a2a8f8509bd9ee037d60de2e3a80847
lunoshist <lucas.noirie@epitech.digital>
2023-03-26 16:14:24 +0200

    tenth version

--- api/chat.js
+++ api/chat.js
@@ -1,9 +1,9 @@
 import fetch from 'node-fetch';
 const apiKey = process.env.API_KEY;
 
-export default async (req, res) => {
-        const body = req.body;
-        return fetch("https://api.openai.com/v1/chat/completions", {
+export default async (request, response) => {
+    const body = req.body;
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
                     method: 'POST',
                     headers: {
                         "Content-Type": "application/json",
@@ -12,4 +12,6 @@ export default async (req, res) => {
                     body: body
                 })
                 .catch((error) => ({ statusCode: 422, body: { message: 'Internal server error :)' + error }}))
-}
+    const data = await response.json();
+    return response.send(data);
+  };
