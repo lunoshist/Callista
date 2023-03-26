bee0dc1d19b7d7affb62e8adcc22049118915c86
lunoshist <lucas.noirie@epitech.digital>
2023-03-26 15:16:13 +0200

    seventh version

--- api/chat.js
+++ api/chat.js
@@ -1,24 +1,12 @@
-const { Configuration, OpenAIApi } = require('openai');
-
-const configuration = new Configuration({
-    apiKey: process.env.API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
 module.exports = async (req, res) => {
-    try {
         const body = req.body;
-        const response = await fetch("https://api.openai.com/v1/chat/completions", {
-            method: 'POST',
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${apiKey}`,
-            },
-            body: body
-        });
-        res.json(body);
-      } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: 'Internal server error' });
-      }
+        return fetch("https://api.openai.com/v1/chat/completions", {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                        "Authorization": `Bearer ${apiKey}`,
+                    },
+                    body: body
+                })
+                .catch((error) => ({ statusCode: 422, body: { message: 'Internal server error :)' + error }}))
 }
